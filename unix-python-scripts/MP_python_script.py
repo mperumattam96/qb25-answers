4c8c2c07844c525d1be84a3c56fb344c5e854fce
mperumattam96 <mperumattam@gmail.com>
2025-09-04 17:29:33 -0400

    Updated python script and unix commands for assignment 3 after class

---
+++ unix-python-scripts/MP_python_script.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import sys
+
+cele_file = open (sys.argv[1]) #cele_file is filestream, cannot be indexed 
+for line in cele_file: #line is iterator 
+    line = line.strip("\n").split("\t") #making each row into a list called "line"
+    gene_length = int(line[2]) - int(line[1])
+    original_score = int(line[4])
+    new_score = original_score*gene_length
+    if line[5] == "-":
+        new_score == new_score * -1
+    if line[5] == "+":
+        new_score == new_score * 1
+    print(line[0], line[1], line[2], line[3], new_score, line[5]) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cele_file = open(sys.argv[1])
+ 
+
+# for bed_data in cele_file:
+#     bed_data = cele_file.strip("\n").split("\t")
+#     start = int(bed_data[1])
+#     end = int(bed_data[2])
+#     gene_length = end - start
+#     original_score = int(bed_data[4])
+#     new_score = original_score*gene_length
+#     if bed_data[5] == "-":
+#         new_score == new_score * -1
+#     if bed_data[5] == "+":
+#         new_score == new_score * 1
+
+
+    
+
+
+
+
+
+
+   
+
+    
+
+
