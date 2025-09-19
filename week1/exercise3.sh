c592bef3d390c5283fd72b60cd15c2f5965e132a
mperumattam96 <mperumattam@gmail.com>
2025-09-19 01:44:09 -0400

    updated exercise 3 bash script

--- week1/exercise3.sh
+++ week1/exercise3.sh
@@ -1,61 +1,47 @@
-(qb25) cmdb@QuantBio-33 Downloads % cd ..
-(qb25) cmdb@QuantBio-33 ~ % cd qb25-answers 
-(qb25) cmdb@QuantBio-33 qb25-answers % cd assignments 
-(qb25) cmdb@QuantBio-33 assignments % ls
-data_exploration_assignment.Rmd
-data_visualization_assignment.Rmd
-MP_data_exploration_assignment_complete
-MP_data_exploration_assignment_complete.Rmd
-qb25-answers_MP:python_dictionaries.ipynb
-qb25-answers:python_basics.ipynb
-(qb25) cmdb@QuantBio-33 assignments % cd ..
-(qb25) cmdb@QuantBio-33 qb25-answers % cd week1 
-(qb25) cmdb@QuantBio-33 week1 % ls
-exercise1.Rmd.R			hg19-main.chrom.sizes
-hg19-1mb.bed			hg19.chrom.sizes
-hg19-kc-count.bed		hgt_genome_23cdbf_4457d0.pdf
-hg19-kc.bed			hgt_genome_23e881_445a20.pdf
-hg19-kc.tsv			nhek.bed
-(qb25) cmdb@QuantBio-33 week1 %   grep 1_Active nhek.bed > nhek-active.bed
-(qb25) cmdb@QuantBio-33 week1 %   grep 12_Repressed nhek.bed > nhek-repressed.bed
-(qb25) cmdb@QuantBio-33 week1 % ls
-exercise1.Rmd.R			hg19.chrom.sizes
-hg19-1mb.bed			hgt_genome_23cdbf_4457d0.pdf
-hg19-kc-count.bed		hgt_genome_23e881_445a20.pdf
-hg19-kc.bed			nhek-active.bed
-hg19-kc.tsv			nhek-repressed.bed
-hg19-main.chrom.sizes		nhek.bed
-(qb25) cmdb@QuantBio-33 week1 % mv /Users/cmdb/Downloads/nhlf.bed /Users/cmdb/qb25-answers/week1
-(qb25) cmdb@QuantBio-33 week1 % ls
-exercise1.Rmd.R			hgt_genome_23cdbf_4457d0.pdf
-hg19-1mb.bed			hgt_genome_23e881_445a20.pdf
-hg19-kc-count.bed		nhek-active.bed
-hg19-kc.bed			nhek-repressed.bed
-hg19-kc.tsv			nhek.bed
-hg19-main.chrom.sizes		nhlf.bed
-hg19.chrom.sizes
-(qb25) cmdb@QuantBio-33 week1 % grep 1_Active nhlf.bed > nhlf-active.bed
-(qb25) cmdb@QuantBio-33 week1 % grep 12_Repressed nhlf.bed > nhlf-repressed.bed
-(qb25) cmdb@QuantBio-33 week1 % ls
-exercise1.Rmd.R			hgt_genome_23e881_445a20.pdf
-hg19-1mb.bed			nhek-active.bed
-hg19-kc-count.bed		nhek-repressed.bed
-hg19-kc.bed			nhek.bed
-hg19-kc.tsv			nhlf-active.bed
-hg19-main.chrom.sizes		nhlf-repressed.bed
-hg19.chrom.sizes		nhlf.bed
-hgt_genome_23cdbf_4457d0.pdf
+cd week1 
+grep 1_Active nhek.bed > nhek-active.bed
+grep 12_Repressed nhek.bed > nhek-repressed.bed
+ls
+mv /Users/cmdb/Downloads/nhlf.bed /Users/cmdb/qb25-answers/week1
+grep 1_Active nhlf.bed > nhlf-active.bed
+grep 12_Repressed nhlf.bed > nhlf-repressed.bed
 (qb25) cmdb@QuantBio-33 week1 % wc nhek-active.bed 
    14013  112104  899705 nhek-active.bed
 (qb25) cmdb@QuantBio-33 week1 % wc nhek-repressed.bed 
    32314  258512 1916982 nhek-repressed.bed
 (qb25) cmdb@QuantBio-33 week1 % wc nhlf-active.bed 
    14888  119104  956585 nhlf-active.bed
-(qb25) cmdb@QuantBio-33 week1 % nhlf-re
-zsh: command not found: nhlf-re
 (qb25) cmdb@QuantBio-33 week1 % wc nhlf-repressed.bed 
    34469  275752 2044657 nhlf-repressed.bed
-(qb25) cmdb@QuantBio-33 week1 % 
+
+#Construct a bedtools command to test where there is any overlap between 1_Active and 12_Repressed in a given condition (aka mutually exclusive)
+cd /Users/cmdb/qb25-answers/week1
+bedtools intersect -u -a nhek-active.bed -b nhek-repressed.bed > mutually_exl.bed
+wc mutually_exl.bed 
+#1 - there is no overlap in nhek active and repressed (output = 0)
+
+#active in NHEK and NHLF = 11608
+bedtools intersect 
+bedtools intersect -u -a nhek-active.bed -b nhlf-active.bed > both_active.bed
+wc both_active.bed
+
+#regions that are active in NHEK but not active in NHLF = 2405
+bedtools intersect -v -a nhek-active.bed -b nhlf-active.bed > NHEKactive_notNHLF.bed
+wc NHEKactive_notNHLF.bed
+
+#features output first command: 11608
+#features output second command: 2405
+#14013 - original number of lines in nhek-active.bed matches the sum
+# 11608+2405 = 14013 ! 
+
+
+
+
+
+
+
+
+
 
 
 
