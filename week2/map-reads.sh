ee14e3f3028d5e1def93efbc79773b666f9b6408
mperumattam96 <mperumattam@gmail.com>
2025-10-06 18:28:06 -0400

    uploading bash script and IGV screenshot of 6 samples

---
+++ week2/map-reads.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for sample in A01_01 A01_02 A01_03 A01_04 A01_05 A01_06
+do 
+    echo "***" ${sample}
+    
+    bowtie2 -p 4 -x ../genomes/sacCer3 -U ~/Data/BYxRM/fastq/${sample}.fq.gz > ${sample}.sam
+    samtools sort -o ${sample}_sort.bam ${sample}.sam
+    samtools index ${sample}_sort.bam
+done 
