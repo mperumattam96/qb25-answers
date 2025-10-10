e98691d65460e0d2df2dc35e81648673b4e657ed
mperumattam96 <mperumattam@gmail.com>
2025-10-09 23:05:33 -0400

    updated exericse 2 with read depth code

--- week3/ex2.py
+++ week3/ex2.py
@@ -2,6 +2,11 @@
 
 import sys
 vcf_file = open("/Users/cmdb/qb25-answers/week3/biallelic.vcf")
+af = open("AF.txt", "w")
+dp = open("DP.txt", "w")
+
+af.write("AlleleFrequency\n")
+dp.write("ReadDepth\n")
 
 for line in vcf_file:
     if line.startswith("#"):
@@ -12,10 +17,23 @@ for line in vcf_file:
     full_allele = info.split(';')[3] #takes only 4th column which is AF
     allele_freq = full_allele.strip("AF=")
     allele_freq = float(allele_freq)
-    print(allele_freq)
+    af.write(f"{allele_freq}\n")
 
-#2.1 
-# It looks roughly like a binomial distribution. This means that most 
-# frequencies are in the intermediate range, which makes sense
-# because the alleles are randomly segregated into the offspring, It is unlikely that
-# all the offspring carry same allele (allele freq=1). 
+    format_fields = fields[8].split(":")
+    samples = fields[9:]
+    if "DP" in format_fields:
+        index = format_fields.index("DP")
+        for position in samples:
+            sample_fields = position.split(':')
+            if len(sample_fields) > index: 
+                depth = float(sample_fields[index])
+                dp.write(f"{depth}\n")
+            else: 
+                continue
+
+
+
+    
+
+
+
