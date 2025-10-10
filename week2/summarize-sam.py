c9a1446e5d811ca4e38c417140e54fa95e3dbf0e
mperumattam96 <mperumattam@gmail.com>
2025-10-09 21:14:36 -0400

    uploading completed exercise 3 from week 2

--- week2/summarize-sam.py
+++ week2/summarize-sam.py
@@ -2,9 +2,9 @@
 
 import sys
 
-sam_file = sys.argv[1]
-
+sam_file = open (sys.argv[1])
 count_alignments = {}
+mismatch_counts = {}
 
 for line in sam_file:
     line = line.rstrip()
@@ -18,5 +18,23 @@ for line in sam_file:
     else:
         count_alignments[rname] = 1
 
+    for field in fields[1:]:
+        if field.startswith("NM:i:"):
+            nm_value = int(field[5:])
+            mismatch_counts[nm_value] = mismatch_counts.get(nm_value, 0) + 1
+            break 
+
+print("Alignments: ")
 for each in count_alignments.keys():
     print(each, count_alignments[each])
+
+print("\nMismatch: ")
+for nm in sorted(mismatch_counts.keys()):
+    print (nm, mismatch_counts[nm])
+
+# examine mismatches per alignment
+# for loop go through line by line and look for NM:i: 
+# and then strip NM and leave it with number
+# key will be the number 
+
+
