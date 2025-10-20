56b063db0d9eddf96ec3f8f905f15bd2c2df0e79
mperumattam96 <mperumattam@gmail.com>
2025-10-20 19:03:11 -0400

    uploading exercise 2 scatterplots for DNMs vs age and step 2.3 code updated

--- week5/week5.R
+++ week5/week5.R
@@ -10,18 +10,39 @@ parental_age_tibble <- as_tibble(parental_age)
 #per-proband summary with counts of maternally and paternally 
 #inherited DNMs.
 
-dnm_tibble %>%
+count <- dnm_tibble %>%
+  group_by(Proband_id) %>%
   summarise(
     DNM_count_father = sum(Phase_combined == "father", na.rm = TRUE),
     DNM_count_mother = sum(Phase_combined == "mother", na.rm = TRUE)
   )
+count
   
-joined <- dnm_tibble %>%
+joined_df <- count %>%
   left_join(parental_age_tibble, by = "Proband_id")
 
+#1) Create a scatter plot of the count of maternal DNMs vs. maternal age 
+# → save as ex2_a.png
 
-lm (data = dnm_tibble, formula = Proband_id ~ 1 + Phase_combined) %>%
+ggplot(joined_df, aes(x = Mother_age  , y = DNM_count_mother )) +
+         geom_point() +
+          labs(
+           x = "Maternal Age",
+           y = "Maternal DNM count",
+         ) 
+
+ggplot(joined_df, aes(x = Father_age  , y = DNM_count_father )) +
+  geom_point() +
+  labs(
+    x = "Paternal Age",
+    y = "Paternal DNM count",
+  ) 
+
+mother_model <- lm (data = joined_df, Mother_age ~ DNM_count_mother) %>%
   summary()
+mother_model
 
-
+father_model <- lm (data = joined_df, Father_age ~ DNM_count_father) %>%
+  summary()
+father_model
 
